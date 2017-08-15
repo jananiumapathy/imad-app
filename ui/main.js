261b4651c82cdb965ec9f85766d822de73531ba6
jananiumapathy <30697358+jananiumapathy@users.noreply.github.com>
2017-08-15 15:48:43 +0530

    [imad-console] Updates ui/main.js

--- ui/main.js
+++ ui/main.js
@@ -1,3 +1,12 @@
 console.log('Loaded!');
 var element=document.getElementById('main-text');
 element.innerHTML = 'New Value';
+var marginLeft=0;
+function moveRight(){
+var marginLeft = marginLeft + 5;
+img.style.marginLeft=marginLeft + 'px';
+}
+img.onclick=function()
+{
+    var interval = setInterval(moveRight,50);
+};
